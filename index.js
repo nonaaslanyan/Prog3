5827a6231bca6520b48857ce8957e3378f4607e5
user <nona.aslanyan@tumo.org>
2023-06-10 15:51:41 +0400

    Tast nodes

---
+++ index.js
@@ -0,0 +1,124 @@
+// console.log("Hello Node");
+
+// var os = require("os");
+
+// var message = "The platform is ";
+
+// function main(){
+
+// console.log(message + os.platform());
+
+// }
+
+// main();
+
+// var express = require("express");
+
+// var app = express();
+
+// app.get("/", function(req, res){
+
+// res.send("Hello world");
+
+// });
+
+// app.listen(3000, function(){
+
+// console.log("Example is running on port 3000");
+
+// });
+
+
+// var express = require("express");
+
+// var app = express();
+
+// app.get("/", function(req, res){
+
+// res.send("<h1>Hello world</h1>");
+
+// });
+
+// app.get("/name/:name", function(req, res){
+
+// var name = req.params.name;
+
+// res.send("<h1>Hello " + name +"</h1>");
+
+// });
+
+// app.listen(4000, function(){
+
+// console.log("Example is running on port 4000");
+
+// });
+
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
+
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//    fs.writeFileSync("dummytext.txt", dummyText);
+//    var text = fs.readFileSync("dummytext.txt").toString();
+//    console.log(dummyText == text);
+//    console.log(text);
+//    fs.writeFileSync("undummytext.txt",
+//        text.replace("Apple", "Microsoft")
+//    );
+// }
+// main();
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/name/:name", function(req, res){
+//     var name = req.params.name;
+//     res.send("<h1>Hello " + name + "</h1>");
+//  });
+ 
+//  app.listen(5000, function(){
+
+// console.log("Example is running on port 5000");
+
+// });
+
+//  var fs = require('fs');
+
+//  function main(){
+//     var file  = "google";
+//     fs.appendFileSync(file, "google.com");
+//  }
+//  main();
+
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.send("hello.txt", "Hello world\n");
+});
+
+app.get("/name/:name", function(req, res){
+    var name = req.params.name;
+    res.send("<h1>Hello " + name +"</h1>");
+ });
+ app.get("/google", function(req, res){
+
+    res.redirect("your_project_html_name");
+    
+    });
+ 
+app.get("/google", function(req, res){
+   res.redirect(" https://google.com/search?q=");
+});
+
+app.listen(6000, function(){
+   console.log("Example is running on port 6000");
+});	
